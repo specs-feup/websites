01c310fd4ff1df88351243ce3ad401fd4762eeca
Sloth32 <17815paulosilva@aeplegua.pt>
2019-05-02 17:37:16 +0100

    array, echo

--- specs-lab-test/assets/php/specs.php
+++ specs-lab-test/assets/php/specs.php
@@ -3,15 +3,42 @@
 /**
  * Tests building the HTML code for the members
  */
-function get_members_test() {
+
+function get_members_test()  {
 
     $members = json_decode(file_get_contents('db/specs_members.json'), true);
 
+    $html = '';
+
     foreach ($members as $member) {
-        echo '<p>' . $member['name'] . '</p>';
 
         if (array_key_exists('ORCID', $member)) {
             echo '<p> ORCID: ' . $member['ORCID'] . '</p>';
+         }
+
+        if (array_key_exists('Nationality', $member)) {
+            echo '<p> Nationality: ' . $member['Nationality'] . '</p>';
+        }
+
+        if (array_key_exists('Email', $member)) {
+            echo '<p> Email: ' . $member['Email '] . '</p>';
+        }
+
+        if (array_key_exists('Web Page', $member)) {
+            echo '<p> Web Page: ' . $member['Web Page'] . '</p>';
+        }
+
+        if (array_key_exists('Affiliation', $member)) {
+            echo '<p> Affiliation: ' . $member['Affiliation'] . '</p>';
+        }
+
+        if (array_key_exists('Position', $member)) {
+            echo '<p> Position: ' . $member['Position'] . '</p>';
+        }
+
+        if (array_key_exists('Web Page', $member)) {
+            echo '<p> Web Page: ' . $member['Web Page'] . '</p>';
         }
     }
-}
+} 
+
