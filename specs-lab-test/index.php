c78f6ddb202d36e3a62bea92d9203cd1ff01748f
JoaoBispo <joaobispo@gmail.com>
2019-07-31 15:16:13 +0100

    [specs-lab-test] Changes 'lab' mentions to 'group'

--- specs-lab-test/index.php
+++ specs-lab-test/index.php
@@ -7,9 +7,9 @@ require_once 'assets/php/specs.php';
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="SPeCS Lab">
-        <meta name="author" content="SPeCS Lab, FEUP, Porto">
-        <title>SPeCS Lab: <?php echo get_title() ?></title>
+        <meta name="description" content="SPeCS Group">
+        <meta name="author" content="SPeCS Group, FEUP, Porto">
+        <title>SPeCS Research Group</title>
 
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,latin-ext,cyrillic,cyrillic-ext' rel='stylesheet' type='text/css'>
 
