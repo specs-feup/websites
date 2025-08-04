6a3523a3b549e3d7a406de937967f97dee9d8f14
João Bispo <jbispo@fe.up.pt>
2025-08-04 18:31:12 +0100

    Adapts example, fixes language of script

--- specs-tools/clava/larajs/CallGraph.js
+++ specs-tools/clava/larajs/CallGraph.js
@@ -1,41 +1,43 @@
-laraImport("weaver.Query")
+import Query from "@specs-feup/lara/api/weaver/Query.js";
 
 // Utility object that counts tuples
-var callGraph = {}
+var callGraph = {};
 
 // Collect information
 for (const $function of Query.search("function")) {
-    for (const $call of Query.searchFrom($function, "call")) {
+  for (const $call of Query.searchFrom($function, "call")) {
+    // Alternatively:
+    //for (const functionCall of Query.search("function").search("call").chain()) {
+    //    const $function = functionCall["function"];
+    //    const $call = functionCall["call"];
 
-        // Alternatively:
-        //for (const functionCall of Query.search("function").search("call").chain()) {
-        //    const $function = functionCall["function"];
-        //    const $call = functionCall["call"];
-
-        // Test 1st key
-        if (!($function.signature in callGraph)) {
-            callGraph[$function.signature] = {};
-        }
-
-        // Test 2nd key
-        if (!($call.signature in callGraph[$function.signature])) {
-            callGraph[$function.signature][$call.signature] = 0;
-        }
-
-        // Increment
-        callGraph[$function.signature][$call.signature]++;
+    // Test 1st key
+    if (!($function.signature in callGraph)) {
+      callGraph[$function.signature] = {};
     }
-}
 
+    // Test 2nd key
+    if (!($call.signature in callGraph[$function.signature])) {
+      callGraph[$function.signature][$call.signature] = 0;
+    }
+
+    // Increment
+    callGraph[$function.signature][$call.signature]++;
+  }
+}
 
 // Create graph in dot format (Graphviz) and print it to the console
-println('// Copy this code, open the web page http://webgraphviz.com/, paste the code and click Generate Graph!');
-println('digraph static_call_graph {\n');
+console.log(
+  "// Copy this code, open the web page http://webgraphviz.com/, paste the code and click Generate Graph!"
+);
+console.log("digraph static_call_graph {\n");
 
-for (f in callGraph) {
-    for (c in callGraph[f]) {
-        println('\t"' + f + '"->"' + c + '" [label="' + callGraph[f][c] + '"];');
-    }
+for (const f in callGraph) {
+  for (const c in callGraph[f]) {
+    console.log(
+      '\t"' + f + '"->"' + c + '" [label="' + callGraph[f][c] + '"];'
+    );
+  }
 }
 
-println('}');
+console.log("}");
